2f28786993ffb5adbe6e89d42e3ae4cf50ef7bd9
Samuel Spiza <samuelspiza@gmail.com>
2010-02-13 17:32:56 +0100

    Rewritten saveFile with File-class

--- moodle.py
+++ moodle.py
@@ -25,6 +25,8 @@ class Course(Thread):
         Thread.__init__(self)
         self.url = url
         self.CourseName = CourseName
+        section = 'moodle-module "' + self.CourseName + '"'
+        self.overrides = uniload.getIndexedOptions(config, section, ["remote", "folder"])
 
     def run(self):
         self.newFiles = []
@@ -62,7 +64,7 @@ class Course(Thread):
     
                 folder = re.sub(u"[^a-zA-Z0-9_()äÄöÖüÜ ]", "", folder).strip()
                 for f in files:
-                    savedFile = self.saveFile(f['href'], folder)
+                    savedFile = self.saveFile(f['href'])
                     if(len(savedFile) > 0):
                         newFiles.append(savedFile)
                 # Folders
@@ -77,64 +79,32 @@ class Course(Thread):
                         newFiles.append(savedFile)
         return newFiles
     
-    def saveFile(self, url, folder = ""):
-        if(folder != ""):
-            folder = "/" + folder
-        isFileNew = False
-        fileChanged = False
-        newPath = "/".join(url.split("/")[5:])
-        #Overrides
-        #config = getConfig()
-        ov = []
-        for section in [s for s in config.sections() if s.startswith("moodle-module ")]:
-            ov.append((section[15:-1],dict(config.items(section))))
-        override = dict(ov)
-        print newPath
-        if os.path.dirname(newPath).lower() not in override[self.CourseName]:
-            fullFileName = os.path.join(self.CourseName, "stuff", newPath)
-        else:
-            fullFileName = os.path.join(self.CourseName, override[self.CourseName][os.path.dirname(newPath).lower()], os.path.basename(newPath))
-        fullFileName = re.sub(r"\?forcedownload=1", "", fullFileName)
-        if not os.path.exists(os.path.dirname(fullFileName)):
-            os.makedirs(os.path.dirname(fullFileName))
-        response = getResponse(url)
-        if(os.path.isfile(fullFileName)):
-            with open(fullFileName, 'r') as f:
-                read_data = f.read()
-                f.closed
-            check_md5 = False
-            # if no Content-Length in header
-            if(response.info().get("Content-Length") is None or check_md5 == True):
-                data = response.read()
-                m1 = hashlib.md5()
-                m2 = hashlib.md5()
-                m1.update(data)
-                m2.update(read_data)
-                #print "Lokal:", m1.hexdigest(), "Online:", m2.hexdigest()
-                if (m1.hexdigest() == m2.hexdigest()):
-                    return []
-                else:
-                    isFileNew = True
-                    fileChanged = True
-            else:
-                #print filename, "check:", "Length"
-                length = response.info().get("Content-Length")
-                if (int(length) == len(read_data)):
-                    return []
-                else:
-                    isFileNew = True
-                    fileChanged = True
-        else:
-            isFileNew = True
-        if(isFileNew == True):
-            data = response.read()
-            with open(fullFileName, "w") as f:
-                f.write(data)
-                f.close()
-            return [fullFileName, url, fileChanged]
+    def saveFile(self, url):
+        # Find local filepath
+        fullFileName = localfile(url)
+
+        # Download file if it was updated
+        f = uniload.File(os.path.basename(fullFileName),
+                         os.path.dirname(url),
+                         os.path.dirname(fullFileName))
+        f.update(test=True)
+
+        # Return if file was updated
+        if f.check():
+            return [f.local, f.web, f.hasChanged()]
         else:
             return []
-        
+
+    def loaclfile(url):
+        newpath = "/".join(url.split("/")[5:])
+        newpath = re.sub(r"\?forcedownload=1", "", newpath)
+        module = self.CourseName
+        for o in self.overrides:
+            if os.path.dirname(newpath) == o['remote']:
+                return os.path.join(module, o['folder'], os.path.basename(newpath))
+        return os.path.join(module, "stuff", newpath)
+                        
+                        
 def finish(newFiles):
     """
     Moved from Monitor class. Sends an email containing all new files.
@@ -161,7 +131,9 @@ def getConfigModules(config, succed):
             if section.startswith("moodle-module ") and section != 'moodle-module "Example"':
                 return True
     config.add_section('moodle-module "Example"')
-    config.set('moodle-module "Example"', "exercise", "uebung")
+    config.set('moodle-module "Example"', "00folder", "uebung")
+    config.set('moodle-module "Example"', "00remote", "exercise")
+    config.set('moodle-module "Example"', "page", "http://moodle.uni-duisburg-essen.de/course/view.php?id=1120")
     return False
 
 def getConfigCredentials(config, succed):
@@ -183,10 +155,7 @@ uniload.uniload()
 
 config = getConfig()
 os.chdir(os.path.expanduser(config.get("uniload", "path")))
-#os.chdir(os.path.expanduser("~/workspace/python/uniload"))
-print os.getcwd()
-#
-#
+
 # Benutzerdaten
 password = config.get("moodle-credentials", "password") # PASSWORD
 user = config.get("moodle-credentials", "user")
@@ -227,16 +196,13 @@ newFiles = {}
 #for link in links:
 #    CourseName = link.string.replace("&amp;", "&")
 #    CourseName = re.sub(u"[^a-zA-Z0-9_() ]", "", CourseName).strip()
-    #print u"Kurs: " + CourseName
-current = Course("http://moodle.uni-duisburg-essen.de/course/view.php?id=219", "rem1")
-courses.append(current)
-current.start()
-current = Course("http://moodle.uni-duisburg-essen.de/course/view.php?id=1965", "dsd")
-courses.append(current)
-current.start()
-current = Course("http://moodle.uni-duisburg-essen.de/course/view.php?id=1120", "bwl")
-courses.append(current)
-current.start()
+#print u"Kurs: " + CourseName
+for section in [s for s in config.sections() if s.startswith("moodle-module ")]:
+    module = section[15:-1]
+    page = config.get(section, "page")
+    current = Course(page, module)
+    courses.append(current)
+    current.start()
     
 for course in courses:
     course.join()
