88925308d5bd0ae336c5e8c0a8b2da5cbc521ee1
Samuel Spiza <samuel.spiza@yahoo.de>
2010-04-17 11:26:21 +0200

    Reboot

---
+++ README.md
@@ -0,0 +1,12 @@
+uniload
+=======
+
+A bulk downloader for moodle and static web sites.
+
+Attributions
+------------
+
+* ### BeautifulSoup ###
+  Copyright (c) 2004-2009 Leonard Richardson.
+  Licensed under New-style BSD.
+  [Website](http://www.crummy.com/software/BeautifulSoup/)
--- uniload.py
+++ uniload.py
@@ -1,78 +1,91 @@
 #!/usr/bin/env python
+#
+# This file is licensed under the Simplified BSD License.
+#
+# Copyright (c) 2009-2010, Samuel Spiza
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+"""uniload - a bulk downloader for moodle and static web sites"""
 
-import os, re, optparse, ConfigParser, sys
-from moodle import openCourse
-from configutil import getIndexedOptions
+__author__ = "Samuel Spiza <sam.spiza@gmail.com>"
+__copyright__ = "Copyright (c) 2009-2010, Samuel Spiza"
+__license__ = "Simplified BSD License"
+__version__ = "0.1"
+
+import sys
+import os
+import re
+import optparse
+import ConfigParser
+from moodlefiles import moodleLogin, openCourse
 from fileupdater import File, absFindall, safe_getResponse
 
-CONFFILES = []
-CONFFILES.append(os.path.expanduser("~/.uniload.conf"))
-CONFFILES.append(os.path.expanduser("~/.uniload-cred.conf"))
-CONFFILES.append(os.path.expanduser("uniload.conf"))
-CONFFILES.append(os.path.expanduser("uniload-cred.conf"))
+CONFIG_FILES = [os.path.expanduser("~/.uniload.conf"),"uniload.conf",
+                os.path.expanduser("~/.uniload-cred.conf"),"uniload-cred.conf"]
 
 def main(argv):
     config = ConfigParser.ConfigParser()
-    config.read(CONFFILES)
+    config.read(CONFIG_FILES)
+    os.chdir(os.path.expanduser(config.get("uniload", "path")))
     options = getOptions(argv)
     config.set("uniload", "test", str(options.test))
     if options.test:
         print "*** TESTMODUS (Keine Filesystemoperationen) ***"
-    uniload(config)
+    uniload(config, options.test)
     moodle(config)
     return 0
 
-def uniload(config):
-    os.chdir(os.path.expanduser(config.get("uniload", "path")))
-    for section in [s for s in config.sections()
-                    if s.startswith("uniload-site ")]:
-        print "Kurs:", section[14:-1]
-        load(config, section)
-
-def moodle(config):
-    os.chdir(os.path.expanduser(config.get("uniload", "path")))
-    
-    # Benutzerdaten
-    password = config.get("moodle-credentials", "password") # PASSWORD
-    user = config.get("moodle-credentials", "user")
-    # CAS Daten
-    casUrl = 'https://cas.uni-duisburg-essen.de/cas/login'
-    casService = "http://moodle.uni-duisburg-essen.de/login/index.php?authCAS=CAS"
-        
-    # Get token
-    data = safe_getResponse(casUrl).read()
-    rawstr = '<input type="hidden" name="lt" value="([A-Za-z0-9_\-]*)" />'
-    token = re.search(rawstr, data, re.MULTILINE).group(1)
-    
-    # Login
-    postData = {'username': user, 'password': password, 'lt': token, '_eventId': 'submit'}
-    safe_getResponse(casUrl + '?service=' + casService, postData)
-    
-    # Use the Service
-    url = 'http://moodle.uni-duisburg-essen.de/index.php'
-    # verueckt, erst muss ich einen kurs aufrufen um in der hauptseite eingeloggt zu sein
-    safe_getResponse("http://moodle.uni-duisburg-essen.de/course/view.php?id=2064")
-    
-    safe_getResponse(url).read()
-        
-    for section in [s for s in config.sections() if s.startswith("moodle-module ")]:
-        module = section[15:-1]
-        page = config.get(section, "page")
-        overrides = getIndexedOptions(config, section, ['regexp', 'remote', 'folder'])
-        openCourse(config, page, module, overrides)
-                
 def getOptions(argv):
     parser = optparse.OptionParser()
     parser.add_option("-t", "--test",
-                      action="store_true", dest="test", default=False)
+                      dest="test", action="store_true", default=False)
     return parser.parse_args(argv)[0]
 
-def load(config, section):
-    module = section[14:-1]
-    page = config.get(section, "page")
+def uniload(config, test):
+    for section in config.sections():
+        if section.startswith("uniload-site "):
+            module = section[14:-1]
+            print "Kurs: %s" % module
+            page = config.get(section, "page")
+            items = getCascadedOptions(config.items(section))
+            load(module, page, items, test)
+
+def moodle(config):
+    moodleLogin(config)
+    for section in config.sections():
+        if section.startswith("moodle-module "):
+            module = section[15:-1]
+            page = config.get(section, "page")
+            overrides = getCascadedOptions(config.items(section))
+            openCourse(config, page, module, overrides)
+                
+def load(module, page, items, test):
     content = safe_getResponse(page).read()
-    test = config.getboolean("uniload", "test")
-    for item in getIndexedOptions(config, section, ["regexp", "folder"]):
+    for item in items:
         localdir = os.path.join(module, item['folder'])
         loaditem(page, item['regexp'], content, localdir, test)
 
@@ -80,6 +93,16 @@ def loaditem(page, regexp, content, localdir, test):
     for remote in absFindall(page, regexp, content=content):
         local = "/".join([localdir, os.path.basename(remote)])
         File(remote, local, test=test).update()
+
+def getCascadedOptions(items, regexp="[0-9]{2}"):
+    options = {}
+    for item in items:
+        m = re.match(regexp, item[0])
+        if m is not None:
+            if m.group(0) not in options:
+                options[m.group(0)] = {}
+            options[m.group(0)][item[0][len(m.group(0)):]] = item[1]
+    return options
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
